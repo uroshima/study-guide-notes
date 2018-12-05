24311cd0c059058d11f14a1b097ebab1aa398b01
urosh <urosh.stojkovikj@gmail.com>
2018-12-04 23:42:38 -0800

    practiced some Strings/Arrays algorithms3

--- algorithms/StringsAndArrays.js
+++ algorithms/StringsAndArrays.js
@@ -52,3 +52,28 @@ function URLify(str1) {
   return result;
 }
 // Time Complexity is O(n)
+
+// Given a string, write a function to check if it is a permutation of a palindrome
+function palindromePermutation(str1) {
+  let myObj = {};
+  let len = str1.length;
+  for (let i = 0; i < str1.length; i++) {
+    if (!myObj[str1[i]]) {
+      myObj[str1[i]] = 1;
+    } else {
+      myObj[str1[i]]++;
+    }
+  }
+  let odds = 0;
+  for (var key in myObj) {
+    if (len % 2 == 0 && myObj[key] % 2 == 1) {
+      return false;
+    } else if (len % 2 == 1 && odds > 1) {
+      return false;
+    } else if (len % 2 == 1 && myObj[key] % 2 == 1) {
+      odds++;
+    }
+  }
+  return true;
+}
+// Time Complexity is O(n + k) ==> O(n)
