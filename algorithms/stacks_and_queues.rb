a53e0bbe70300f6da66bea1d5f9ae391e5f19204
urosh <urosh.stojkovikj@gmail.com>
2018-12-13 18:54:34 -0800

    stack/queues algorithms practice SetOfStacks without popAt method

--- algorithms/stacks_and_queues.rb
+++ algorithms/stacks_and_queues.rb
@@ -2,6 +2,8 @@
 # which returns the minimun element? All the functions should operate in O(1) time
 # Solution: We gonna use additional stack to keep track of the min element
 class Stack
+  attr_reader :elements, :length
+
   def initialize
     @elements = []
     @length = 0
@@ -15,7 +17,7 @@ class Stack
     @elements
   end
 
-  def pop()
+  def pop
     last = @elements.pop()
     @length -= 1
     @mins.pop() if last == self.min
@@ -25,5 +27,40 @@ class Stack
   def min
     return nil if @mins.empty?
     @mins.last
+  end
+end
+
+
+# SetOfStacks should be composed of several stacks and should create a new stack
+# once the previous one exceeds capacity.Also implement a function popAt(index)
+# which performs a pop operation on a specific sub-stack.
+class SetOfStacks
+  attr_reader :current_stack, :set
+  def initialize(capacity)
+    @current_stack = Stack.new
+    @set = []
+    @capacity = capacity
+    @set << @current_stack
+  end
+
+  def push(el)
+    if @current_stack.length == @capacity
+      @current_stack = Stack.new
+      @set << @current_stack
+    end
+    @current_stack.push(el)
+    @set
+  end
+
+  def pop
+    if @current_stack.empty?
+      @set.pop()
+      @current_stack = @set.last
+    end
+    @current_stack.pop()
+  end
+
+  def popAt(index)
+    @set[index].pop()
   end
 end
